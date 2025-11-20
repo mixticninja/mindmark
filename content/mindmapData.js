4d6865c7afb29fec317235cb5fd71a0f456c343b
MixticStudio <mixticstudio@gmail.com>
2025-11-20 04:41:19 +0100

    demo data

--- content/mindmapData.js
+++ content/mindmapData.js
@@ -1,210 +1,38 @@
 const mindmapData = {
-    "label": "Stratégies de Recherche d'Emploi dans le Numérique",
+    "label": "What would be a good mindmap exploration tool ?",
     "children": [
         {
-            "label": "Le Marché de l'Emploi IT (2024-2025)",
+            "label": "Should be responsive and fast",
             "children": [
                 {
-                    "label": "Tendances Générales",
-                    "children": [
-                        { "label": "Forte Baisse des Offres (-18% en 2024-2025)" },
-                        { "label": "Retour au Niveau d'Avant Covid (Post-Accélération)" },
-                        { "label": "Recherche de Candidats Plus Qualifiés/Expérimentés" },
-                        { "label": "Forte Évolution due à l'IA et la Cybersécurité" },
-                        {"label": "Offres", "link":"content/offres.html" }
-                    ]
+                    "label": "Template design",
                 },
                 {
-                    "label": "Difficultés Spécifiques",
-                    "children": [
-                        { "label": "Alternance: Fortement Réduite (-20% de signatures)" },
-                        { "label": "Compétition Internationale (Inde, Ukraine)" },
-                        { "label": "Sourcing Moins Prévisionnel (Moins de 'Pipeline')" },
-                        { "label": "No Code Impacte Postes (Surtout Front)" }
-                    ]
+                    "label": "Self contained,serveur less",
                 },
+            ]
+        },
+        {
+            "label": "Should be possible to get it from all sort of data",
+            "children": [
                 {
-                    "label": "Métiers Demandés (Selon Expérience)",
-                    "children": [
-                        {
-                            "label": "Profils Juniors (0-3 ans)",
-                            "children": [
-                                { "label": " Technicien Support (Porte d'entrée)" },
-                                { "label": "Petites Entreprises (Alternance/Emploi)" }
-                            ]
-                        },
-                        {
-                            "label": "Profils Intermédiaires/Seniors",
-                            "children": [
-                                { "label": "Développeur (Niveau Intermédiaire)" },
-                                { "label": "Consultants (Postes très demandés)" },
-                                { "label": "Chef de Projet" },
-                                { "label": "Cybersécurité (Profils Experts)" }
-                            ]
-                        },
-                        {
-                            "label": "Spécificités Locales (Toulouse)",
-                            "children": [
-                                { "label": "Secteur Industriel (Airbus, Aéronautique)" },
-                                { "label": "Développement Back End Privilégié (vs Front End)" }
-                            ]
-                        },
-                        {
-                            "label": "Les métiers",
-                            "link": "content/metier.html"
-                        }
-                    ]
-                },
-                 {
-                    "label": "Attentes du marché",
-                    "link": "content/marche.html"
+                    "label": "Simple data structure",
                 }
             ]
         },
         {
-            "label": "L'Écosystème Numérique (Réseau)",
+            "label": "Should be linked to external information",
             "children": [
                 {
-                    "label": "Acteurs Clés (Partenaires)",
+                    "label": "Links pages can present information at the right time",
                     "children": [
+                        { "label": "read this first " },
                         {
-                            "label": "La Mêlée (Réseau Associatif du Numérique)",
-                            "children": [
-                                { "label": "Facilite les Rencontres (350+ événements/an)" },
-                                { "label": "Festival de la Mélée Numérique (9800 pers.)" },
-                                { "label": "Favorise la Cooptation" }
-
-                            ]
-                        },
-                        {
-                            "label": "Numéum (Syndicat des Entreprises du Numérique)",
-                            "children": [
-                                { "label": "Représente la Filière" },
-                                { "label": "Entretiens Numèric Emploi (Conseil CV/Parcours)" }
-                            ]
-                        },
-                        {
-                            "label": "Digital 113 (Cluster / Annuaire IA)",
-                            "children": [
-                                { "label": "Réseau Entreprises (300+ adhérents)" },
-                                { "label": "Factories (Groupes de travaill thématiques)" },
-                                { "label": "Annuaire IA" }
-
-                            ]
-                        },
-                        {
-                            "label": "CyberOcc (Centre Régional de Cybersécurité)",
-                            "children": [
-                                { "label": "Accompagnement et Ressources Pédagogiques" },
-                                { "label": "Service d'Urgence Cyber (Hotline)" },
-                                { "label": "MOOC ANSSI recommandé" }
-
-                            ]
-
-                        },
-                        {
-                            "label": "ATLAS (OPCO/Alternance)",
-                            "children": [
-                                { "label": "Financeur de la Formation/Alternance" },
-                                { "label": "Collecteur de Données Marché" }
-                            ]
-                        },
-                         {
-                            "label": "Développer mon réseau",
-                            "link": "content/reseau.html"
+                            "label": "More information",
+                            "link": "content/foad.html"
                         }
                     ]
-
-                },
-                {
-                    "label": "Écosystèmes Start-up (Villages)",
-                    "children": [
-                        { "label": "French Tech/loT Valley / Village by CA / La Cité / At Home" },
-                        { "label": "Ouverture d'Esprit/Recrutement Moins Frileux" },
-                        { "label": "Bonne source pour Annuaire d'Entreprises" },
-                        { "label": "Exemples: Careo, Orbital, Bleu 122 (Dev/Front)" }
-                    ]
-
-                },
-               {
-                    "label": "Partenaires pour l'Emploi",
-                    "children": [
-                        { "label": "France Travail / APEC / Cap Emploi / Mission Locale" }
-                    ]
-
-                },
-                {
-                    "label": "Annuaires entreprises",
-                    "link":"content/annuaire.html"
-
-                }  
-            ]
-        },
-        {
-            "label": "Conseils et Actions de Candidature",
-            "children": [
-                {
-                    "label": "Optimisation du CV et Profil",
-                    "children": [
-                        { "label": "Adapter CV aux Métiers Ciblés (Mots-clés)" },
-                        { "label": "Mettre en Avant Compétences en Cybersécurité (Transversale)" },
-                        { "label": "Développer Polyvalence (Élargir Compétences)" },
-                        {
-                            "label": "Formation en ligne",
-                            "link": "content/foad.html"
-
-                        },
-                        { "label": "Format CV type ESN (Expérience, Missions, Environnement Tech)" },
-                        { "label": "Importance des Réalisations (Projets personnels, Github)" },
-                         {
-                            "label": "Partager mes projets",
-                            "link": "content/projets.html"
-
-                        }  
-                    ]
-                },
-                {
-                    "label": "Stratégies Proactives (Marché Caché)",
-                    "children": [
-                        { "label": "Le Réseau est le 'Nerf de la Guerre (80% des recrutements)'" },
-                        { "label": "Participer aux Événements/Salons (Meetup, Defest, Afterworks)" },
-                        { "label": "Candidatures Spontanées (via Annuaires des Partenaires)" },
-                        { "label": "Veille Economique (Levées de Fonds, Evolution du secteur)" },
-                        { "label": "Enquêtes Professionnelles (Connaître le Contexte Entreprise)" }
-                    ]
-                },
-                {
-                    "label": "Visibilité et Outils",
-                    "children": [
-                        {
-                            "label": "Linkedin (95% des recruteurs consultent avant entretien)",
-                            "children": [
-                                { "label": "Mettre en avant Certifications/Technos" },
-                                { "label": "Entretenir le Réseau (Ajouter les contacts)" }
-                            ]
-                        },
-                        { "label": "Sites Spécialisés (Welcome to the Jungle, Digital Skills)" },
-                        { "label": "Activer la Promotion de Profil (France Travail)" }
-                    ]
-                },
-                {
-                    "label": "Ouverture de Voies",
-                    "children": [
-                        { "label": " Vérifier les pages Carrière d'Entreprises (Marché Ouvert)" },
-                        { "label": "Cibler les Entreprises 'Hors Numérique (MSA, Banques, Assurances)" },
-                        { "label": "Considérer le Portage Salarial" }
-                    ]
-                },
-                {
-                            "label": "Formation en centre",
-                            "link": "content/formation.html"
-
-                },
-                {
-                            "label": "Liens utiles",
-                            "link": "content/debreef.html"
-
-                } 
+                }
             ]
         }
     ]
